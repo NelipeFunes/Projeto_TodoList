ed83a5e86c0a942f134e2a759e12f7d3c3ec490f
NelipeFunes <miotarestaurante2003@gmail.com>
2022-01-05 20:13:57 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const btn = document.getElementById("criar-tarefa");
-const list = document.getElementById("lista-tarefas");
+const btn = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
 
-function createTask(){
-let task = document.getElementById("texto-tarefa");
-let temp = document.createElement('li');
-temp.innerHTML = task.value;
-list.appendChild(temp);
-task.value = '';
+function changeColor() {
+  this.classList.add('selected');
 }
 
+function createTask() {
+  const task = document.getElementById('texto-tarefa');
+  const temp = document.createElement('li');
+  temp.innerHTML = task.value;
+  list.appendChild(temp);
+  task.value = '';
+  temp.addEventListener('click', changeColor);
+}
 
-btn.addEventListener('click',createTask);
+btn.addEventListener('click', createTask);
